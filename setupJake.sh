302fef2816471804202bcd5bb4419d9f4a5f0985
altonbill <jacobbills14@gmail.com>
2021-10-04 13:39:33 -0600

    Add files via upload

---
+++ setupJake.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This is a simple script to set up ubuntu the way I like it.
+
+# update & upgrade
+echo 'Performing system update'
+sudo apt-get update
+sudo apt-get -y upgrade
+echo 'Done.'
+
+# install apps from software-repos
+echo 'Installing favourite applications'
+sudo apt-get install -y git-core emacs xclip htop python3-pip nettools gnuradio traceroute texlive-latex-recommended
+echo 'Done.'
+
+# Configure git
+echo 'Configuring git'
+git config --global user.name "bill3"
+git config --global user.email "jacob.bills@utah.edu"
+git config --global alias.acm '!git add . && git commit -m'
+echo 'Done.'
+
